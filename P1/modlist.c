0d100817e3aa771bcc4e094949b668e116d86214
tombe85 <mhtombe85@gmail.com>
2015-10-17 21:42:27 +0200

    Actualizado modlist.c con comentarios y mejoras

--- P1/modlist.c
+++ P1/modlist.c
@@ -6,42 +6,58 @@
 #include <asm-generic/uaccess.h>
 #include <linux/ftrace.h>
 
-
-
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Modlist Kernel Module - FDI-UCM");
 MODULE_AUTHOR("Miguel Higuera Romero & Alejandro Nicolás Ibarra Loik");
 
-#define BUFFER_LENGTH       PAGE_SIZE
 #define BUF_LEN 100
 
+/* Entrada de proc */
 static struct proc_dir_entry *proc_entry;
-static char *modlist;  // Space for the "modlist" buffer
 
+/* Lista enlazada */
+struct list_head mylist; 
 
-struct list_head mylist; /* Lista enlazada */
 /* Nodos de la lista */
 typedef struct {
   int data;
   struct list_head links;
 } list_item_t;
 
-
-void init_mylist(void) {
-  INIT_LIST_HEAD(&mylist);
+/* Función que limpia la lista entera */
+void cleanUpList(void){
+	list_item_t *mynodo;
+	struct list_head* cur_node=NULL;
+    struct list_head* aux=NULL;
+    
+    /* Recorremos la lista */
+    list_for_each_safe(cur_node, aux, &mylist) {
+      mynodo = list_entry(cur_node,list_item_t, links);
+      /* Eliminamos nodo de la lista */
+      list_del(cur_node);
+      
+      /*Liberamos memoria dinámica del nodo */
+      vfree(mynodo);
+    }
+    
+    /* Informamos */
+	printk(KERN_INFO "Modlist: List cleaned up\n");
 }
 
+/* Función write */
 static ssize_t modlist_write(struct file *filp, const char __user *buf, size_t len, loff_t *off) {
   int available_space = BUF_LEN-1;
-  int num;
-  char cadena[BUF_LEN];
-  list_item_t *mynodo;
-
-  if ((*off) > 0) /* The application can write in this entry just once !! */
+  int num;					//Numero leído en add/remove
+  char cadena[BUF_LEN];		//Cadena escrita en /proc
+  char modlist[BUF_LEN];	//Copia de buffer de usuario
+  list_item_t *mynodo;		//nodo a añadir/eliminar
+	
+  /* The application can write in this entry just once !! */
+  if ((*off) > 0) 
     return 0;
 
   if (len > available_space) {
-    printk(KERN_INFO "modlist: not enough space!!\n");
+    printk(KERN_INFO "Modlist: not enough space!!\n");
     return -ENOSPC;
   }
 
@@ -49,67 +65,67 @@ static ssize_t modlist_write(struct file *filp, const char __user *buf, size_t l
   if (copy_from_user( &modlist[0], buf, len ))
     return -EFAULT;
 
-  modlist[len] = '\0'; /* Add the `\0' */
+  modlist[len] = '\0'; 	/* Add the '\0' */
   *off+=len;            /* Update the file pointer */
-
+	
+  /* Leemos ordenes */
   if(sscanf(&modlist[0],"add %i",&num)) {
-    //printk(KERN_INFO "Se ha leido add %i\n",num);
 
     /* Creamos el nuevo nodo */
     mynodo = (list_item_t*) vmalloc(sizeof(list_item_t));
+    
     /* Guardamos el valor leido */
     mynodo->data = num;
+    
     /* Añadimos el nodo a la lista */
     list_add_tail(&mynodo->links, &mylist);
-    mynodo = NULL;
-    printk(KERN_INFO "Aniadido el elemento %i a la lista\n", num);
+    
+    /* Informamos */
+    printk(KERN_INFO "Modlist: Added element %i to the list\n", num);
 
-  }
-  else if(sscanf(&modlist[0],"remove %i",&num)) {
+  }else if(sscanf(&modlist[0],"remove %i",&num)) {
 
-    list_item_t *item=NULL;
     struct list_head* cur_node=NULL;
     struct list_head* aux=NULL;
-
+    int deletes = 0;
+	
+	/* Para cada nodo de la lista comprobamos si contiene num */
     list_for_each_safe(cur_node, aux, &mylist) {
-      /* item points to the structure wherein the links are embedded */
-      item = list_entry(cur_node,list_item_t, links);
-      if(item->data == num) {
-        printk(KERN_INFO "Borrando elemento %i\n", item->data);
+      mynodo = list_entry(cur_node,list_item_t, links);
+      if(mynodo->data == num) {
+		/* Eliminamos de la lista */
         list_del(cur_node);
-        vfree(item);
+        
+        /* Liberamos memoria dinámica */
+        vfree(mynodo);
+        
+        /* Mostramos info del kernel una sola vez*/
+        if(deletes == 0){
+			printk(KERN_INFO "Modlist: Element %i deleted from list\n", num);
+			deletes++;
+		}
       }
     }
-  }
-  else if(sscanf(&modlist[0],"%s", cadena) && strcmp("cleanup", cadena)==0) {
-    struct list_head* cur_node=NULL;
-    struct list_head* aux=NULL;
-    list_for_each_safe(cur_node, aux, &mylist) {
-      /* item points to the structure wherein the links are embedded */
-      mynodo = list_entry(cur_node,list_item_t, links);
-      printk(KERN_INFO "Borrando elemento %i\n", mynodo->data);
-      list_del(cur_node);
-      vfree(mynodo);
-    }
-
-  }
-  else {
+  }else if(sscanf(&modlist[0],"%s", cadena) && strcmp("cleanup", cadena)==0) {
+	
+	/* Limpiamos la lista entera */
+    cleanUpList();
+    
+  }else {
     // retornar un codigo de error correspondiente (instruccion incorrecta)
     return -EINVAL;
   }
-
-  //trace_printk("Current value of modlist: %s\n",modlist);
-
   return len;
 }
 
+/* Función read */
 static ssize_t modlist_read(struct file *filp, char __user *buf, size_t len, loff_t *off) {
 
-  int nr_bytes;
-  list_item_t *item=NULL;
+  int nr_bytes;						// Bytes leídos
+  list_item_t *item=NULL;			//Nodo a leer
   struct list_head* cur_node=NULL;
-  char msg[BUF_LEN];
-  char msgtmp[BUF_LEN];
+  char msg[BUF_LEN];				//Mensaje final
+  char msgtmp[BUF_LEN];				//Mensajes temporales
   msg[0] = '\0';
 
   if ((*off) > 0) /* Tell the application that there is nothing left to read */
@@ -117,29 +133,29 @@ static ssize_t modlist_read(struct file *filp, char __user *buf, size_t len, lof
 
   /* Listamos los elementos de la lista */
   list_for_each(cur_node, &mylist) {
-    /* item points to the structure wherein the links are embedded */
     item = list_entry(cur_node,list_item_t, links);
+    
+    /* Escribimos en msgtmp el dato, el \n y lo concatenamos al msg final */
     sprintf(msgtmp, "%i", item->data);
     strcat(msgtmp, "\n");
     strcat(msg, msgtmp);
   }
   strcat(msg, "\0");
-
-  printk(KERN_INFO "Listando elementos %s\n", msg);
-
+	
+  /* Cargamos los bytes leídos */
   nr_bytes=strlen(msg);
-  printk(KERN_INFO "Numero de bytes de msg %i\n", nr_bytes);
 
-  if (len<nr_bytes)
-    return -ENOSPC;
+  /*if (len<nr_bytes)
+    return -ENOSPC;*/
 
-    /* Transfer data from the kernel to userspace */
+  /* Enviamos datos al espacio de ususario */
   if (copy_to_user(buf, msg, nr_bytes))
     return -EINVAL;
 
-  printk(KERN_INFO "Datos espacio de usuario %s\n", buf);
-
   (*off)+=len;  /* Update the file pointer */
+  
+  /* Informamos */
+  printk(KERN_INFO "Modlist: Elements listed\n");
 
   return nr_bytes;
 }
@@ -153,39 +169,32 @@ static const struct file_operations proc_entry_fops = {
 
 int init_modlist_module( void )
 {
+	/* Creamos la entrada de proc */
+	proc_entry = proc_create( "modlist", 0666, NULL, &proc_entry_fops);
+	if (proc_entry == NULL) {
+	  printk(KERN_INFO "Modlist: Can't create /proc entry\n");
+	  return -ENOMEM;
+	} else {
+	  printk(KERN_INFO "Modlist: Module loaded\n");
+	}
 
-  /* Recervamos memoria dinamica */
-  modlist = (char *)vmalloc( BUFFER_LENGTH );
+	/* Inicializamos la lista */
+	INIT_LIST_HEAD(&mylist);
 
-  if (!modlist) {
-    return -ENOMEM;
-  } else {
-
-    /* Asignamos la variable en memoria dinamica como variable compartida */
-    //memset( modlist, 0, BUFFER_LENGTH );
-    proc_entry = proc_create( "modlist", 0666, NULL, &proc_entry_fops);
-    if (proc_entry == NULL) {
-      vfree(modlist);
-      printk(KERN_INFO "modlist: Can't create /proc entry\n");
-      return -ENOMEM;
-    } else {
-      printk(KERN_INFO "modlist: Module loaded\n");
-    }
-
-    /* Inicializamos la lista */
-    init_mylist();
-  }
-
-  return 0;
-
+	return 0;
 }
 
 
 void exit_modlist_module( void )
 {
+  /* Limpiamos la lista para liberar memoria */
+  cleanUpList();
+  
+  /* Eliminamos la entrada de proc */
   remove_proc_entry("modlist", NULL);
-  vfree(modlist);
-  printk(KERN_INFO "modlist: Module unloaded.\n");
+  
+  /* Informamos */
+  printk(KERN_INFO "Modlist: Module unloaded.\n");
 }
 
 
