9d806d5766049784f7470506414c6c703dde1a4e
Miguel Higuera <miguel@debiantmb.deb.tmb>
2015-10-29 15:28:40 +0100

    Arreglado open de cpuinfo.c

--- P2/cpuinfo.c
+++ P2/cpuinfo.c
@@ -8,10 +8,10 @@
 
 #define BUFSIZE 512
 
-#ifndef SYS_read 
-#define SYS_read          0 
+#ifndef SYS_read
+#define SYS_read          0
 #endif
-#ifndef SYS_open 
+#ifndef SYS_open
 #define SYS_open          0
 #endif
 
@@ -21,33 +21,33 @@ int main(void)
     int read_chars=0;
     int fd=0;
     char buf[BUFSIZE+1];
-    
+
     /* Open /proc entry in read-only mode */
     //fd=open("/proc/cpuinfo",O_RDONLY);
-    
-    if((fd=syscall(SYS_open,"/proc/cpuinfo",O_CREAT | O_RDWR,0666)) < 0) {
+
+    if((fd=syscall(SYS_open,"/proc/cpuinfo", O_RDONLY,0666)) < 0) {
       fprintf(stderr,"Error while reading the file\n");
       exit(1);
     }
-    
+
     if (fd<0){
         fprintf(stderr,"Can't open the file\n");
         exit(1);
     }
-    
+
     /* Loop that reads data from the file and prints its contents to stdout */
     while((read_chars=syscall(SYS_read,fd,buf,BUFSIZE))>0){
         buf[read_chars]='\0';
-        printf("%s",buf);            
+        printf("%s",buf);
     }
-     
+
     if (read_chars<0){
         fprintf(stderr,"Error while reading the file\n");
-        exit(1);        
+        exit(1);
     }
-    
-    	
-    /* Close the file and exit */ 
+
+
+    /* Close the file and exit */
     close(fd);
     return 0;
 }
